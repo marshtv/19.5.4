2e1db92b7b7b092289e2e603840c2bf689747634
marshtv <marshtv.1972@gmail.com>
2022-12-13 23:46:28 +0300

    Start

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main() {
+	std::ifstream payroll;
+	payroll.open("F:\\__C++\\Skillbox\\19\\19.5.3\\Files\\Payroll.txt");
+
+	return 0;
+}
