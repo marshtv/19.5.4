f21fa72c6cfd46114957bbc8b2e52582a5c316d7
marshtv <marshtv.1972@gmail.com>
2022-12-14 13:57:30 +0300

    Complete

--- main.cpp
+++ main.cpp
@@ -3,8 +3,45 @@
 #include <string>
 
 int main() {
-	std::ifstream payroll;
-	payroll.open("F:\\__C++\\Skillbox\\19\\19.5.3\\Files\\Payroll.txt");
+	std::string file_path;
+	std::cout << "Input path to file for PNG-check:" << std::endl;
+	std::cin >> file_path;
 
-	return 0;
+	//std::cout << file_path.substr((file_path.size() - 3));
+	std::string file_ext = file_path.substr((file_path.size() - 3));
+
+	if (file_ext != "png") {
+		std::cout << "File's extension is not a *.png" << std::endl;
+		std::cout << "Check Failed." << std::endl;
+		return 0;
+	}
+
+	std::ifstream check_file;
+	check_file.open(file_path);
+	if (!check_file.is_open()) {
+		std::cout << "File's path ERROR. File didn't opened" << std::endl;
+		return 0;
+	}
+
+	int8_t check_num;
+	char buffer[3];
+	std::string check_ext_str;
+
+	check_file.read((char*)buffer, 0);
+	check_file >> check_num;
+	//std::cout << (int)check_num << std::endl;
+
+	check_file.read((char*)buffer, 2);
+	check_file >> buffer;
+	//std::cout << buffer << std::endl;
+
+	if ((int)check_num != -119 && buffer[0] != 'P' && buffer[1] != 'N' && buffer[2] != 'G') {
+		std::cout << "Content's identification ERROR!!!" << std::endl;
+		std::cout << "Check Failed." << std::endl;
+		return 0;
+	} else {
+		std::cout << "Check OK." << std::endl;
+		std::cout << "File is PNG." << std::endl;
+		return 0;
+	}
 }
